62167cdcec125c3c86206300929c2015f7f137fc
Benjamim <benjamim_c@hotmail.com>
2024-11-06 16:57:07 -0700

    deploy

---
+++ script.js
@@ -0,0 +1,49 @@
+Wolf.addEventListener("click",function(){
+    console.log("Wolf");
+    document.getElementById("image").src = "wolf.webp";
+    document.getElementById("Wolf").style.backgroundColor="#97bf04";
+    document.getElementById("Moose").style.backgroundColor="#d9043d";
+    document.getElementById("Elk").style.backgroundColor="#d9043d";
+    document.getElementById("Bear").style.backgroundColor="#d9043d";
+    document.getElementById("Sheep").style.backgroundColor="#d9043d";
+});
+
+Moose.addEventListener("click",function(){
+    console.log("Moose");
+    document.getElementById("image").src = "moose.webp";
+    document.getElementById("Moose").style.backgroundColor="#97bf04";
+    document.getElementById("Wolf").style.backgroundColor="#d9043d";
+    document.getElementById("Elk").style.backgroundColor="#d9043d";
+    document.getElementById("Bear").style.backgroundColor="#d9043d";
+    document.getElementById("Sheep").style.backgroundColor="#d9043d";
+});
+
+Elk.addEventListener("click",function(){
+    console.log("Elk");
+    document.getElementById("image").src = "elk.webp";
+    document.getElementById("Elk").style.backgroundColor="#97bf04";
+    document.getElementById("Wolf").style.backgroundColor="#d9043d";
+    document.getElementById("Moose").style.backgroundColor="#d9043d";
+    document.getElementById("Bear").style.backgroundColor="#d9043d";
+    document.getElementById("Sheep").style.backgroundColor="#d9043d";
+});
+
+Bear.addEventListener("click",function(){
+    console.log("Bear");
+    document.getElementById("image").src = "bear.webp";
+    document.getElementById("Bear").style.backgroundColor="#97bf04";
+    document.getElementById("Wolf").style.backgroundColor="#d9043d";
+    document.getElementById("Moose").style.backgroundColor="#d9043d";
+    document.getElementById("Elk").style.backgroundColor="#d9043d";
+    document.getElementById("Sheep").style.backgroundColor="#d9043d";
+});
+
+Sheep.addEventListener("click",function(){
+    console.log("Sheep");
+    document.getElementById("image").src = "sheep.webp";
+    document.getElementById("Sheep").style.backgroundColor="#97bf04";
+    document.getElementById("Wolf").style.backgroundColor="#d9043d";
+    document.getElementById("Moose").style.backgroundColor="#d9043d";
+    document.getElementById("Elk").style.backgroundColor="#d9043d";
+    document.getElementById("Bear").style.backgroundColor="#d9043d";
+});
